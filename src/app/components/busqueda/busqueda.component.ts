8b6e2e5e80d40e70c4e02d828cc6a4e4d117a4f6
CARLOS EDUARDO GONZALEZ CORTES <EXTIGCEGONZALEZC@compensar.com>
2017-06-27 16:26:16 -0500

    Se implementa funcionalidad de busqeda

--- src/app/components/busqueda/busqueda.component.ts
+++ src/app/components/busqueda/busqueda.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PeliculasService } from "app/services/peliculas.service";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-busqueda',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class BusquedaComponent implements OnInit {
-
-  constructor() { }
+  public texto:string;
+  constructor(private peliculaService:PeliculasService,
+              private activatedRoute:ActivatedRoute) { }
 
   ngOnInit() {
+  }
+
+  onclick(){
+    this.getPelicula();
+  }
+
+  getPelicula(){
+    this.peliculaService.buscarPelicula(this.texto).subscribe(peliculas =>{
+      console.log(peliculas);
+    });
   }
 
 }
