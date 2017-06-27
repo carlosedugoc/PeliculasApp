6a888ac69465c53d935d1322b735f3520565dfba
CARLOS EDUARDO GONZALEZ CORTES <EXTIGCEGONZALEZC@compensar.com>
2017-06-27 14:23:35 -0500

    Se crea componente de galeria y pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,22 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule, JsonpModule } from '@angular/http';
 
+//componentes
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { BusquedaComponent } from './components/busqueda/busqueda.component';
 import { NavComponent } from './components/nav/nav.component';
+import { PeliculaComponent } from './components/pelicula/pelicula.component';
 
+//servicios
 import { PeliculasService } from './services/peliculas.service';
 
+//rutas
 import {APP_ROUTING} from './app.routes';
-import { PeliculaComponent } from './components/pelicula/pelicula.component';
+
+//pipes
+import { PeliculaImagenPipe } from './pipes/pelicula-imagen.pipe';
+import { GaleriaComponent } from './components/home/galeria.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +26,9 @@ import { PeliculaComponent } from './components/pelicula/pelicula.component';
     HomeComponent,
     BusquedaComponent,
     NavComponent,
-    PeliculaComponent
+    PeliculaComponent,
+    PeliculaImagenPipe,
+    GaleriaComponent
   ],
   imports: [
     BrowserModule,
